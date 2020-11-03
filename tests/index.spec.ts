988e42c43274d1db0f89256770293f0959eecc52
KiKiKi <chaika.design@gmail.com>
2020-11-04 00:55:07 +0900

    :rotating_light: Add test

---
+++ tests/index.spec.ts
@@ -0,0 +1,27 @@
+import getOuterSize from '../src/index';
+
+let divElm: null | HTMLElement = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  divElm = document.createElement('div');
+  document.body.appendChild(divElm);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  divElm && divElm.remove();
+  divElm = null;
+});
+
+describe('getOuterSize', () => {
+  describe('return width & height including margin', () => {
+    it('from has margin element', () => {
+      divElm.setAttribute('style', 'width: 100px; height: 100px; margin: 10px 5px 5px 10px');
+
+      const { width, height } = getOuterSize(divElm);
+
+      expect(width).toBe(10 + 5);
+      expect(height).toBe(10 + 5);
+    });
+  });
+});
