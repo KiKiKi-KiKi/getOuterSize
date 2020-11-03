94c45952c85b4009dcf742f62d18c8a6061e770d
KiKiKi <chaika.design@gmail.com>
2020-11-04 01:45:27 +0900

    :beetle: Fix element.getBoundingClientRect return value always 0 in jest.

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -5,6 +5,18 @@ beforeEach(() => {
   // setup a DOM element as a render target
   divElm = document.createElement('div');
   document.body.appendChild(divElm);
+  Element.prototype.getBoundingClientRect = jest.fn(() => {
+    return {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    } as DOMRect;
+  });
 });
 
 afterEach(() => {
@@ -16,17 +28,21 @@ afterEach(() => {
 describe('getOuterSize', () => {
   describe('return width & height including margin', () => {
     it('from has margin element', () => {
-      divElm.setAttribute('style', 'width: 100px; height: 100px; margin: 10px 5px 5px 10px');
+      divElm.setAttribute('style', 'margin: 10px 5px 5px 10px');
 
       const { width, height } = getOuterSize(divElm);
 
-      expect(width).toBe(10 + 5);
-      expect(height).toBe(10 + 5);
+      expect(width).toBe(100 + 10 + 5);
+      expect(height).toBe(100 + 10 + 5);
     });
   });
 
   it('from no margin element', () => {
-    divElm.setAttribute('style', 'width: 100px; height: 100px;');
+    const { width, height } = getOuterSize(divElm);
+
+    expect(width).toBe(100);
+    expect(height).toBe(100);
+  });
 
     const { width, height } = getOuterSize(divElm);
 
